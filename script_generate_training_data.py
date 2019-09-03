7a8280be67e0495d014764eece21e4004c954437
Heiko Schuett <heiko.schuett@nyu.edu>
2019-09-03 10:52:39 -0400

    update training data script

--- script_generate_training_data.py
+++ script_generate_training_data.py
@@ -15,11 +15,11 @@ dl.save_training_data('validation',10000)
 
 ## separate by exponent
 for i in range(1,6):
-    dl.save_training_data('training%d' % i,100000)
-    dl.save_training_data('validation%d' % i,10000)
+    dl.save_training_data('training%d' % i,100000,exponents=np.array(i))
+    dl.save_training_data('validation%d' % i,10000,exponents=np.array(i))
     
 ## full range of distances
 distances = np.arange(2,250)
 distancesd = np.arange(2,250)
 dl.save_training_data('trainingAll',100000,distances=distances,distancesd=distancesd)
-dl.save_training_data('validationAll',10000)
+dl.save_training_data('validationAll',10000,distances=distances,distancesd=distancesd)
